00328fab490d34414db8ee29bc67e546e5bf13d1
Yarramneni-vasavi <Yarramneni-vasavi@missionrnd.com>
2016-03-15 21:10:47 -0700

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -12,6 +12,36 @@ NOTES: Don't create new string.
 #include <Stdio.h>
 #include <string.h>
 
-void str_words_in_rev(char *input, int len){
-	
+void WordReverseStr(char *, int, int);
+void str_words_in_rev(char *input, int len)
+{
+	int start = 0, end;
+	for (int i = 0; i < len; i++)
+	{
+		if (input[i] == ' ')
+		{
+			end = i - 1;
+			WordReverseStr(input, start, end);
+			start = i + 1;
+		}
+		if (input[i + 1] == '\0')
+		{
+			end = i;
+			WordReverseStr(input, start, end);
+		}
+	}
+	WordReverseStr(input, 0, len - 1);
+	printf("%s", input);
+}
+void WordReverseStr(char *input, int start, int end)
+{
+	char temp;
+	while (start < end)
+	{
+		temp = input[start];
+		input[start] = input[end];
+		input[end] = temp;
+		++start;
+		--end;
+	}
 }
