f518ff05539eb897cb2b83a7259a4457ef17309b
Yarramneni-vasavi <Yarramneni-vasavi@missionrnd.com>
2016-03-16 23:45:21 -0700

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -17,6 +17,76 @@ NOTES: If there are no common words return NULL.
 
 #define SIZE 31
 
-char ** commonWords(char *str1, char *str2) {
+char *StringCopy(char *, char *, int);
+
+char **commonWords(char *str1, char *str2)
+{
+	if (str1 != NULL && str2 != NULL)
+	{
+		int index = 0, flag;
+		char **temp = (char **)malloc(sizeof(char*)*SIZE);
+		int start = 0, end;
+		for (int i = 0; str1[i] != '\0'; i++)
+		{
+			//identifying each word end index in str1 string
+			if (str1[i] != ' ' && str1[i + 1] == ' ' || str1[i] != ' ' && str1[i + 1] == '\0')
+			{
+				end = i;
+				temp[index] = (char *)malloc(sizeof(char)*(end - start));
+				int start2 = 0, end2;
+				for (int j = 0; str2[j] != '\0'; j++)
+				{
+					if (str2[j] != ' ' && str2[j + 1] == ' ' || str2[j] != ' ' && str2[j + 1] == '\0')
+					{
+						end2 = j;	flag = 0;
+
+						int s1 = start, s2 = start2;
+						while (s1 <= end || s2 <= end2)
+						{
+
+							if (str1[s1] != str2[s2])
+							{
+								flag = 1; break;
+							}
+							s1++;	s2++;
+						}
+						if (flag == 0)
+						{
+							//copying the matched word to temp 2D array
+							StringCopy(temp[index], (str2 + start2), end2 - start2);
+							index++;
+							break;
+						}
+					}
+					if (str2[j] == ' ' && str2[j + 1] != ' ')
+					{
+						start2 = j + 1;
+					}
+				}
+			}
+			//identifying each word start index in str1 string
+			if (str1[i] == ' ' && str1[i + 1] != ' ')
+			{
+				start = i + 1;
+			}
+		}
+		if (index == 0)
+			return NULL;
+		else
+			return temp;
+	}
 	return NULL;
+}
+
+char *StringCopy(char *destination, char *source, int len)
+{
+	int i;
+	char *temp1;
+	temp1 = destination;
+	for (i = 0; i <= len; i++)
+	{
+		*(temp1 + i) = *(source + i);
+	}
+	temp1[i] = '\0';
+	return temp1;
 }
